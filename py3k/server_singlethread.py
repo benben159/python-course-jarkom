bdd220cb09225b78e99c472604bdd947b800f0c3
Benediktus Anindito <bennybroz105@gmail.com>
2018-01-16 19:18:31 +0700

    add py3k example code

---
+++ py3k/server_singlethread.py
@@ -0,0 +1,20 @@
+import socket
+
+sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock_server.bind(('127.0.0.1',9999))
+sock_server.listen(1)
+client, addr = sock_server.accept()
+print('koneksi client diterima: {}'.format(addr))
+while True:
+    #try:
+    #client.send('hello client')
+    balasan = client.recv(1024).decode('utf-8') ## 1024 adalah jumlah character yg diterima
+    balasan = 'anda menulis: {}'.format(balasan)
+    balasan = balasan.encode('utf-8')
+    client.send(balasan)
+    #except Exception as e:
+    #    break
+    #except KeyboardInterrupt as k:
+    #    break
+
---
+++ py3k/simple_socket_client.py
@@ -0,0 +1,14 @@
+import socket
+
+sock_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_client.connect(('127.0.0.1',9999))
+while True:
+    try:
+        message = input('tulis pesan ke server: ')
+        sock_client.send(message)
+        print(sock_client.recv(1024).decode('utf-8'))
+    except Exception as e:
+        print(e)
+        break
+    except KeyboardInterrupt as k:
+        break
